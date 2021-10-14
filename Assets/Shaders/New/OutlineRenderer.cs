97ed366d61fc212d6027302f7ed1536387ece603
Dane <Byrd>
2021-10-14 16:37:18 -0500

    Rewrote pass found in Shaders/New, imported HDRP color pyramid (blur)

---
+++ Assets/Shaders/New/OutlineRenderer.cs
@@ -0,0 +1,155 @@
+﻿// This is a modified version of a Renderer Feature written by Harry Heath.
+//      Twitter: https://twitter.com/harryh___h/status/1328024692431540224
+//      Pastebin: https://pastebin.com/rvju9psM
+// The original code was used in a recreation of a Mako illustration:
+//      https://twitter.com/harryh___h/status/1328006632102526976
+
+using Shaders.CustomColorBufferOutlines;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+// ReSharper disable MemberInitializerValueIgnored
+
+namespace Shaders.New
+{
+    public enum RenderQueueType
+    {
+        Opaque = 0,
+        Transparent = 1
+    }
+
+    [System.Serializable]
+    public class OutlineSettings
+    {
+        [HideInInspector] public string profilerTag = nameof(OutlineRenderer);
+
+        struct outline_obj
+        {
+            private Vector3 position;
+            private uint unity_InstanceID;
+        }
+
+        public RenderPassEvent renderPassEvent;
+        public RenderQueueType renderQueueType;
+
+        [Space(10)] [Header("Linework Settings")]
+        public PassSubTarget colorTarget = new("Outline", true, RenderTextureFormat.ARGBFloat);
+
+        public PassSubTarget depthTarget = new("Outline", true, isDepth: true);
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Space(10)] [Header("ComputeLines")]
+        public ComputeShader computeBlur;
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Space(10)] [Header("Blit")]
+        public Material blitMaterial;
+        public Shader outlineEncoder;
+
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+        [Space(10)] [Header("Shader Properties")] [Tooltip("Object Threshold.")]
+        public float outerThreshold;
+
+        [Tooltip("Inner Threshold.")] public float innerThreshold;
+
+        [Tooltip("Rotations.")] public int rotations;
+        [Tooltip("Depth Push.")] public float depthPush;
+
+        [Tooltip("Object LUT.")] public Texture2D outerLUT;
+        [Tooltip("Inner LUT.")] public Texture2D innerLUT;
+
+        // Initialize outline settings.
+        private OutlineSettings(string name, PassSubTarget color, PassSubTarget depth, ComputeShader gaussianBlur, RenderQueueType type,
+            RenderPassEvent passEvent,
+            Material blitMat, Shader outlineEncode)
+        {
+            profilerTag = name;
+            colorTarget = color;
+            depthTarget = depth;
+            computeBlur = gaussianBlur;
+            renderQueueType = type;
+            renderPassEvent = passEvent;
+            blitMaterial = blitMat;
+            outlineEncoder = outlineEncode;
+            outerThreshold = 1.0f;
+            innerThreshold = 1.0f;
+            rotations = 8;
+            depthPush = 1e-6f;
+        }
+    }
+
+    public class OutlineRenderer : ScriptableRendererFeature
+    {
+        public OutlineSettings outlineSettings;
+
+        private ShaderPassToRT _lineworkPass;
+
+        private FullscreenQuadRenderer _computeLinesAndBlitPass;
+
+        private Material _outlineEncoderMaterial;
+        private Shader outlineEncoderShader => outlineSettings.outlineEncoder;
+
+        private static readonly int HhoOuterThreshold = Shader.PropertyToID("_HHO_OuterThreshold");
+
+        private static readonly int HhoInnerThreshold = Shader.PropertyToID("_HHO_InnerThreshold");
+
+        private static readonly int HhoRotations = Shader.PropertyToID("_HHO_Rotations");
+        private static readonly int HhoDepthPush = Shader.PropertyToID("_HHO_DepthPush");
+        private static readonly int HhoOuterLut = Shader.PropertyToID("_HHO_OuterLUT");
+        private static readonly int HhoInnerLut = Shader.PropertyToID("_HHO_InnerLUT");
+
+        public OutlineRenderer()
+        {
+        }
+
+
+        public override void Create()
+        {
+            var s = outlineSettings;
+
+            _lineworkPass = new ShaderPassToRT(s.profilerTag, s.colorTarget, s.depthTarget, s.renderPassEvent, s.renderQueueType, 24);
+            _computeLinesAndBlitPass = new FullscreenQuadRenderer("Outline Encoder");
+            GetMaterial();
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            var s = outlineSettings;
+            if (!GetMaterial())
+            {
+                Debug.LogErrorFormat(
+                    "{0}.AddRenderPasses(): Missing material. Make sure to add a blit material, or make sure {1} exists.",
+                    GetType().Name, outlineEncoderShader);
+                return;
+            }
+
+            Shader.SetGlobalFloat(HhoOuterThreshold, s.outerThreshold);
+            Shader.SetGlobalFloat(HhoInnerThreshold, s.innerThreshold);
+            Shader.SetGlobalInt(HhoRotations, s.rotations);
+            Shader.SetGlobalFloat(HhoDepthPush, s.depthPush);
+            Shader.SetGlobalTexture(HhoOuterLut, s.outerLUT);
+            Shader.SetGlobalTexture(HhoInnerLut, s.innerLUT);
+
+            renderer.EnqueuePass(_lineworkPass);
+            _computeLinesAndBlitPass.Init(_outlineEncoderMaterial, "_OutlineTexture", true);
+            renderer.EnqueuePass(_computeLinesAndBlitPass);
+        }
+
+        private bool GetMaterial()
+        {
+            if (_outlineEncoderMaterial && outlineSettings.blitMaterial)
+            {
+                return true;
+            }
+
+            if (outlineEncoderShader == null || !outlineSettings.blitMaterial) return false;
+            _outlineEncoderMaterial = new Material(outlineEncoderShader);
+            return true;
+        }
+    }
+}
---
+++ Assets/Shaders/New/ShaderPassToRT.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Shaders.New
+{
+    [System.Serializable]
+    public class PassSubTarget
+    {
+        public string shaderName;
+        public RenderTargetIdentifier targetIdentifier;
+        public RenderTargetHandle targetHandle;
+        public bool createTexture;
+        public RenderTextureFormat _renderTextureFormat;
+
+        public PassSubTarget(string shaderName, bool createTexture, RenderTextureFormat renderTextureFormat)
+        {
+            targetIdentifier = new RenderTargetIdentifier(shaderName);
+            targetHandle = new RenderTargetHandle(targetIdentifier);
+            targetHandle.Init(targetIdentifier);
+        }
+        
+        public PassSubTarget(string shaderName, bool createTexture, bool isDepth)
+        {
+            targetIdentifier = new RenderTargetIdentifier(shaderName);
+            targetHandle = new RenderTargetHandle(targetIdentifier);
+            targetHandle.Init(targetIdentifier);
+            if (isDepth)
+            {
+                _renderTextureFormat = RenderTextureFormat.Depth;
+            }
+        }
+        
+    }
+
+    public class ShaderPassToRT : ScriptableRenderPass
+    {
+        #region Variables
+        private CustomColorBufferOutlines.OutlineSettings _outlineSettings;
+        private ComputeShader _computeShader;
+
+        private RenderQueueType _renderQueueType;
+        private FilteringSettings _filteringSettings;
+        private string _profilerTag;
+
+        private ShaderTagId _shaderTagId;
+        private int _textureDepth;
+
+        private PassSubTarget _colorConfig;
+        private RenderTargetIdentifier ColorTargetId => _colorConfig.targetIdentifier;
+        private RenderTargetHandle ColorHandle => _colorConfig.targetHandle;
+        private bool CreateColorTexture => _colorConfig.createTexture;
+        private RenderTextureFormat ColorFormat => _colorConfig._renderTextureFormat;
+
+        private PassSubTarget _depthConfig;
+        private RenderTargetHandle DepthHandle => _depthConfig.targetHandle;
+        private RenderTargetIdentifier DepthTargetId => _depthConfig.targetIdentifier;
+        private bool CreateDepthTexture => _depthConfig.createTexture;
+        private RenderTextureFormat DepthFormat => _depthConfig._renderTextureFormat;
+        #endregion
+        
+        public ShaderPassToRT(string profilerTag,
+                              PassSubTarget colorConfig,
+                              PassSubTarget depthConfig,
+                              RenderPassEvent renderPassEvent,
+                              RenderQueueType renderQueueType,
+                              int depth)
+        {
+            
+            _profilerTag = profilerTag;
+            
+            _colorConfig = colorConfig;
+            _depthConfig = depthConfig;
+            this.renderPassEvent = renderPassEvent;
+            _renderQueueType = renderQueueType;
+            var renderQueueRange = (renderQueueType == RenderQueueType.Opaque)
+                ? RenderQueueRange.opaque
+                : RenderQueueRange.transparent;
+            _filteringSettings = new FilteringSettings(renderQueueRange, ~0);
+            _textureDepth = depth;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            
+            if (CreateColorTexture)
+            {
+                cmd.GetTemporaryRT(ColorHandle.id, cameraTextureDescriptor.width, cameraTextureDescriptor.height, _textureDepth, FilterMode.Point,
+                    ColorFormat);
+            }
+
+            if (CreateDepthTexture)
+            {
+                cmd.GetTemporaryRT(DepthHandle.id, cameraTextureDescriptor.width, cameraTextureDescriptor.height, _textureDepth, FilterMode.Point,
+                    DepthFormat);
+            }
+
+            ConfigureTarget(ColorTargetId, DepthTargetId);
+
+            // cmd.CopyTexture(DepthTargetId, (int)RenderTextureSubElement.Depth,ColorTargetId, (int)RenderTextureSubElement.Depth);
+
+            if (CreateColorTexture || CreateDepthTexture)
+            {
+                ConfigureClear(ClearFlag.All, Color.black);
+            }
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var camData = renderingData.cameraData.camera;
+            Vector4 camSize = new Vector4(camData.scaledPixelWidth, camData.scaledPixelHeight, 0, 0);
+
+            CommandBuffer cmd = CommandBufferPool.Get(_profilerTag);
+
+            SortingCriteria sortingCriteria = _renderQueueType == RenderQueueType.Opaque
+                ? renderingData.cameraData.defaultOpaqueSortFlags
+                : SortingCriteria.CommonTransparent;
+            DrawingSettings drawingSettings = CreateDrawingSettings(_shaderTagId, ref renderingData, sortingCriteria);
+
+            context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref _filteringSettings);
+
+            cmd.SetComputeTextureParam(_computeShader, 0, "_Source", ColorTargetId, 0, RenderTextureSubElement.Color);
+            cmd.SetComputeTextureParam(_computeShader, 0, "_Source", DepthTargetId, 0, RenderTextureSubElement.Depth);
+            cmd.SetComputeVectorParam(_computeShader, "_Size", camSize);
+            cmd.DispatchCompute(_computeShader, 0, 8, 8, 1);
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            if (CreateColorTexture) cmd.ReleaseTemporaryRT(ColorHandle.id);
+            if (CreateDepthTexture) cmd.ReleaseTemporaryRT(DepthHandle.id);
+        }
+    }
+}
