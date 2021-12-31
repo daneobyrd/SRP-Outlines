9f588af011360197334bb28e139d675d497ec45a
Dane Byrd <daneobyrd@gmail.com>
2021-12-31 16:40:22 -0600

    Early WIP on property drawer

---
+++ Assets/Plugins/RenderPass/OutlineBuffers/CustomPassTarget.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[Serializable]
+public enum CustomTargetType
+{
+    Color,
+    Depth,
+    Shadowmap
+}
+
+[Serializable]
+public class PassSubTarget
+{
+    public CustomTargetType customTargetType;
+    public List<string> lightModeTags;
+    public string textureName;
+    [NonSerialized] public int renderTargetInt;
+    [NonSerialized] public RenderTargetIdentifier targetIdentifier;
+    public bool createTexture;
+    public RenderTextureFormat renderTextureFormat;
+
+    public PassSubTarget(List<string> lightModeTags, string texName, CustomTargetType type, bool createTexture, RenderTextureFormat rtFormat)
+    {
+        customTargetType      = type;
+        this.lightModeTags = lightModeTags;
+        textureName        = texName;
+        renderTargetInt    = Shader.PropertyToID(textureName);
+        targetIdentifier   = new RenderTargetIdentifier(renderTargetInt);
+        this.createTexture = createTexture;
+        renderTextureFormat = customTargetType switch
+        {
+            CustomTargetType.Color     => rtFormat,
+            CustomTargetType.Depth     => RenderTextureFormat.Depth,
+            CustomTargetType.Shadowmap => RenderTextureFormat.Shadowmap,
+            _                       => rtFormat
+        };
+    }
+
+    public PassSubTarget(List<string> lightModeTags, string texName, CustomTargetType type, bool createTexture)
+    {
+        customTargetType      = type;
+        this.lightModeTags = lightModeTags;
+        textureName        = texName;
+        renderTargetInt    = Shader.PropertyToID(textureName);
+        targetIdentifier   = new RenderTargetIdentifier(renderTargetInt);
+        this.createTexture = createTexture;
+        renderTextureFormat = customTargetType switch
+        {
+            CustomTargetType.Color     => RenderTextureFormat.Default,
+            CustomTargetType.Depth     => RenderTextureFormat.Depth,
+            CustomTargetType.Shadowmap => RenderTextureFormat.Shadowmap,
+            _                       => RenderTextureFormat.Default
+        };
+    }
+}
---
+++ Assets/Plugins/RenderPass/OutlineBuffers/Editor/ShaderPassToRTDrawerAttribute.cs
@@ -0,0 +1,18 @@
+﻿/*using System;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Tells a ShaderPassToRTDrawer which ShaderPassToRT class is intended for the GUI inside the ShaderPassToRTDrawer class
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class ShaderPassToRTDrawerAttribute : Attribute
+{
+    internal Type targetPassType;
+
+    /// <summary>
+    /// Indicates that the class is a Custom Pass drawer and that it replaces the default Custom Pass GUI.
+    /// </summary>
+    /// <param name="targetPassType">The Custom Pass Target type.</param>
+    public ShaderPassToRTDrawerAttribute(Type targetPassType) => this.targetPassType = targetPassType;
+}*/
